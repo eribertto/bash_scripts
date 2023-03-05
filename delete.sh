54ba910dc79d6bdffab3ef382c5e939b362e4440
erimendz_fed36_cinammon <erimendz@gmail.com>
2023-03-05 06:35:11 +0300

    add delete.sh script

---
+++ delete.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: delete.sh
+#
+#         USAGE: ./delete.sh filename
+#
+#   DESCRIPTION: a not-so-cunning file deletion utility
+#
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Eriberto Mendez
+#  ORGANIZATION:
+#       CREATED: March 5/23
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+E_BADARGS=85
+if [ -z "$1" ]
+then
+    echo "Usage: $(basename "$0") filename"
+    exit $E_BADARGS
+else
+    file=$1
+fi
+
+## this is the short hand AND LIST to give error if file is non-existent
+[ ! -f "$file" ] && echo "The filename \"$file\" is not found!"
+## OR LIST to delete the file if present
+[ ! -f "$file" ] || (rm -f "$file"; echo "File \"$file\" is deleted!")
+exit $?
+

a8830594c19bd6053fcaed426fed93c1335564c2
eriberttom_eos-t460 <erimendz@gmail.com>
2024-07-21 06:06:30 +0300

    refine the code

--- perl/guess_number_game.pl
+++ perl/guess_number_game.pl
@@ -1,25 +1,28 @@
 #!/usr/bin/env perl
-use v5.38;
-use utf8;
 
-# Guess the number game
-# https://code-maven.com/slides/perl/solution-number-guessing-game
+use Modern::Perl '2024';
 
-my $N = 200; # constant number
+  #use v5.38;
+  #use utf8;
+
+  # Guess the number game
+  # https://code-maven.com/slides/perl/solution-number-guessing-game
+
+my $N = 200;    # constant number
 my $hidden = 1 + int rand $N;
-print "Please guess the number from 1 to $N> ";
-my $guess = <STDIN>;
+print "Please guess the number from 1 to $N ($hidden)> ";
+my $guess = <ARGV>;
 chomp $guess;
 
 # do a loop
-if ($guess < $hidden) {
+if ( $guess < $hidden ) {
     print "$guess is too low\n";
-    print "The magic number is $hidden."
+    print "The magic number is $hidden.";
 }
-if ($guess > $hidden) {
+if ( $guess > $hidden ) {
     print "$guess is too high\n";
-    print "The magic number is $hidden."
+    print "The magic number is $hidden.";
 }
-if ($guess == $hidden) {
+if ( $guess == $hidden ) {
     print "Eureka! $guess is the magic number!";
-    }
+}

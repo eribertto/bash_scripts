b633592d2ba24ec2dcc8ea48a7abbc6892ee9e0d
eriberttom_eos-t460 <erimendz@gmail.com>
2024-08-09 11:05:31 +0300

    add number range to the prompt

--- perl/prompter.pl
+++ perl/prompter.pl
@@ -5,14 +5,14 @@ use v5.38;
 use warnings;
 use IO::Prompter;
 
-my $score = prompt 'Enter score:', -number => sub{ 0 <= $_ && $_ <= 100 };
-
+my $score = prompt 'Enter score between 1-100:',
+  -number => sub { 0 <= $_ && $_ <= 100 };
 
 # This call has no automatically added options...
 my $assent = prompt "Do you wish to take the test?", -yn;
 
 {
-    use IO::Prompter [-yesno, -single, -style=>'bold'];
+    use IO::Prompter [ -yesno, -single, -style => 'bold' ];
 
     # These three calls all have: -yesno, -single, -style=>'bold' options
     my $ready = prompt 'Are you ready to begin?';
@@ -22,7 +22,4 @@ my $assent = prompt "Do you wish to take the test?", -yn;
 
 # This call has no automatically added options...
 scalar prompt 'Type any key to start...', -single;
-
-
-
 

7eac8301c7c21d090393bc14c4437ec46936aea4
eriberttom_fed40 <erimendz@gmail.com>
2024-08-26 15:44:00 +0300

    refactor print.output pl

--- perl/PbExample/print.output.pl
+++ perl/PbExample/print.output.pl
@@ -9,7 +9,7 @@ use warnings;
 
 print "Hello, world!\n";
 print "Hello,", " world!\n";
-print ("It's a perfect day!\n"); # parens optional
+print ("It's a perfect day today!\n"); # parens optional
 print "The date and time now: ", localtime, "\n";
 printf "Hello, world\n";
 printf("Meet %s%:Age 5d%:Salary \$10.2f\n", "John", 40, 5500);
